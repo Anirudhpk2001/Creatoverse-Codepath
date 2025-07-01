147e76d1fafc7803de6aa1ab39c8303bea698e2a
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-07-02 02:37:58 +0530

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const URL = 'https://grhtmhmdnrkdkxmqhvkm.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyaHRtaG1kbnJrZGt4bXFodmttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3Mjk0NjQsImV4cCI6MjA2NjMwNTQ2NH0.zgok_ixUVij29gTnjXqNkdvox4rcG-F8TwFZ_Ol2xi8'; 
+const URL = 'your api url';
+const API_KEY = 'your api key'; 
 export const supabase = createClient(URL, API_KEY);
 
 
